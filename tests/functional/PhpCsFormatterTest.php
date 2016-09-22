e345b5e67523cfa63aca5b9ccb946991e1ac9feb
Gerard Brull <gbrull@trovit.com>
2016-09-22 20:18:10 +0200

    Fix tests

--- tests/functional/PhpCsFormatterTest.php
+++ tests/functional/PhpCsFormatterTest.php
@@ -26,7 +26,7 @@ class PhpCsFormatterTest extends \PHPUnit_Framework_TestCase
 
     public function testFormatCode()
     {
-        $code = file_get_contents(__DIR__.'/../Resources/PhpCodeFiles/badIndentationCode.txt');
+        $code = file_get_contents(__DIR__.'/../resources/PhpCodeFiles/badIndentationCode.txt');
         $formattedCode = $this->sut->formatCode($code);
         $expectedCode = file_get_contents(__DIR__.'/../Resources/PhpCodeFiles/expectedBadIndentationCode.txt');
         $this->assertEquals($expectedCode, $formattedCode);
@@ -34,7 +34,7 @@ class PhpCsFormatterTest extends \PHPUnit_Framework_TestCase
 
     public function testFormatCodeAlreadyFormattedCode()
     {
-        $code = file_get_contents(__DIR__.'/../Resources/PhpCodeFiles/expectedBadIndentationCode.txt');
+        $code = file_get_contents(__DIR__.'/../resources/PhpCodeFiles/expectedBadIndentationCode.txt');
         $formattedCode = $this->sut->formatCode($code);
         $this->assertEquals($code, $formattedCode);
     }
